6f200ac403ea52876740ef49a5e5d29bc9e94319
Sl1sar <122501745+Sl1sar@users.noreply.github.com>
2024-04-08 00:29:53 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -5,6 +5,15 @@ import "math"
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
 
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
